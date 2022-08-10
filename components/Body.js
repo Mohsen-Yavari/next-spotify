9e2242985776ea7b03dc4b2f303a01aaa99ae245
Mohsen-Yavari <mohsenyavari713@gmail.com>
2022-08-10 15:47:40 +0330

    talwind scrolbar hide our talwind scrolbar install

--- components/Body.js
+++ components/Body.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import Search from "./Search"
 
 function Body() {
   return (
-    <div>Body</div>
+    <div className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
+      <Search />
+    </div>
   )
 }
 
