fbe8d6e79d743a21f58f4e4b79e3526f5a3325be
Mohsen-Yavari <mohsenyavari713@gmail.com>
2022-08-13 14:50:47 +0330

    create poster components for song

--- components/Body.js
+++ components/Body.js
@@ -3,9 +3,11 @@ import Search from "./Search";
 
 import { useSession } from "next-auth/react";
 
+import Poster from "./Poster";
 
 
-function Body({spotifyApi}) {
+
+function Body({chooseTrack,spotifyApi}) {
   const { data: session } = useSession();
   const {accessToken} = session;
   const [search,setSearch] = useState("");
@@ -62,7 +64,25 @@ useEffect(() => {
       <Search search={search} setSearch={setSearch} />
       <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2
       lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4">
-
+        {searchResults.length === 0
+          ? newReleases
+              .slice(0, 4)
+              .map((track) => (
+                <Poster
+                  key={track.id}
+                  track={track}
+                  chooseTrack={chooseTrack}
+                />
+              ))
+          : searchResults
+              .slice(0, 4)
+              .map((track) => (
+                <Poster
+                  key={track.id}
+                  track={track}
+                  chooseTrack={chooseTrack}
+                />
+              ))}
       </div>
     </div>
   )
